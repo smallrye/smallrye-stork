ee89427b9199cca4d6f5f82d4daba075b83ca46b
Gytis Trikleris <gytis@redhat.com>
2021-05-12 15:45:24 +0200

    Delete unused modules

--- api/src/main/java/io/smallrye/loadbalancer/TargetAddressProvider.java
+++
@@ -1,8 +0,0 @@
-package io.smallrye.loadbalancer;
-
-import java.util.List;
-import java.util.concurrent.CompletionStage;
-
-public interface TargetAddressProvider {
-    CompletionStage<List<TargetAddress>> getAddressList();
-}
--- api/src/main/java/io/smallrye/loadbalancer/core/Constants.java
+++
@@ -1,5 +0,0 @@
-package io.smallrye.loadbalancer.core;
-
-public class Constants {
-    public static final String LOAD_BALANCER_PREFIX = "io.smallrye.loadbalancer";
-}
