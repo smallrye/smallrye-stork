09babb57a011223f148bb8f9ea2f3fc7ef484242
Clement Escoffier <clement@apache.org>
2021-10-14 09:24:03 +0100

    Deployed 1588ee0 to 1.0.0.Alpha7 with MkDocs 1.2.2 and mike 1.1.2

---
+++ 1.0.0.Alpha7/snippets/examples/StorkServiceSelectionExample.java
@@ -0,0 +1,26 @@
+package examples;
+
+import io.smallrye.stork.Service;
+import io.smallrye.stork.ServiceInstance;
+import io.smallrye.stork.Stork;
+
+import java.time.Duration;
+import java.util.List;
+
+public class StorkServiceSelectionExample {
+
+    public static void main(String[] args) {
+        Stork.initialize();
+        Stork stork = Stork.getInstance();
+
+        Service service = stork.getService("my-service");
+        ServiceInstance instance = service.selectServiceInstance()
+                .await().atMost(Duration.ofSeconds(5));
+
+        System.out.println(instance.getHost() + ":" + instance.getPort());
+
+        // ...
+        Stork.shutdown();
+    }
+
+}
