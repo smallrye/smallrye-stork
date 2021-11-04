aee22e33289f0c4c2fb1990540805d08d64e5ece
Auri Munoz <amunozhe@redhat.com>
2021-11-04 15:15:02 +0100

    fix: fix the path to the file containing the version Fixes #133

--- .github/render-documentation.sh
+++ .github/render-documentation.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-STORK_VERSION=$(cat .github/project.yml | yq eval '.release.current-version' -)  && mike deploy --push --update-aliases $STORK_VERSION latest --branch gh-pages
-STORK_VERSION=$(cat .github/project.yml | yq eval '.release.current-version' -)  && mike set-default --push latest
+STORK_VERSION=$(cat project.yml | yq eval '.release.current-version' -)  && mike deploy --push --update-aliases $STORK_VERSION latest --branch gh-pages
+STORK_VERSION=$(cat project.yml | yq eval '.release.current-version' -)  && mike set-default --push latest
