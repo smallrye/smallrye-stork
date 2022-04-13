89ed342eac3bce60b5d369ec744c9e6a9d111859
Auri Munoz <amunozhe@redhat.com>
2022-04-13 17:47:22 +0200

    doc: fix some docs things

--- service-discovery/kubernetes/src/main/java/io/smallrye/stork/servicediscovery/kubernetes/KubernetesServiceDiscoveryProvider.java
+++ service-discovery/kubernetes/src/main/java/io/smallrye/stork/servicediscovery/kubernetes/KubernetesServiceDiscoveryProvider.java
@@ -14,7 +14,7 @@ import io.vertx.core.Vertx;
 @ServiceDiscoveryType("kubernetes")
 @ServiceDiscoveryAttribute(name = "k8s-host", description = "The Kubernetes API host.")
 @ServiceDiscoveryAttribute(name = "k8s-namespace", description = "The namespace of the service. Use all to discover all namespaces.")
-@ServiceDiscoveryAttribute(name = "application", description = "The Eureka application Id; if not defined Stork service name will be used.")
+@ServiceDiscoveryAttribute(name = "application", description = "The Kubernetes application Id; if not defined Stork service name will be used.")
 @ServiceDiscoveryAttribute(name = "refresh-period", description = "Service discovery cache refresh period.", defaultValue = "5M")
 @ServiceDiscoveryAttribute(name = "secure", description = "Whether the connection with the service should be encrypted with TLS.")
 public class KubernetesServiceDiscoveryProvider
