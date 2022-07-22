c8de1a2e11bad5a71d8f866d9cb72525c3168504
Clement Escoffier <clement@apache.org>
2022-07-22 09:50:01 +0200

    Update web site
    - update mkdocs
    - use themes (dark vs. light)
    - update images and diagrams to render correctly based on the theme
    - move the mkdocs.yaml in the docs directory and restructure docs (like mutiny)
    - use pipenv and a Pipfile
    - intregrate docissimo macros

--- .github/render-documentation.sh
+++ .github/render-documentation.sh
@@ -5,6 +5,6 @@ DIR="$(dirname $(realpath "$0"))"
 echo $DIR
 
 export STORK_VERSION=$(cat "$DIR/project.yml" | yq eval '.release.current-version' -)
-
+cd docs
 mike deploy --push --update-aliases $STORK_VERSION latest --branch gh-pages
 mike set-default --push latest
