6695fd04602824e0c2f48ac21784b8033e5d1b93
Auri Munoz <amunozhe@redhat.com>
2022-12-29 19:32:45 +0100

    RoundRobinLobalancer Test as CDI beans and a few refactors

--- core/src/test/java/io/smallrye/stork/EmptyLoadBalancerProvider.java
+++ core/src/test/java/io/smallrye/stork/EmptyLoadBalancerProvider.java
@@ -1,4 +1,4 @@
-package io.smallrye.stork.test;
+package io.smallrye.stork;
 
 import io.smallrye.stork.api.LoadBalancer;
 import io.smallrye.stork.api.ServiceDiscovery;
--- core/src/test/java/io/smallrye/stork/WeirdTypedLoadBalancerProvider.java
+++ core/src/test/java/io/smallrye/stork/WeirdTypedLoadBalancerProvider.java
@@ -1,4 +1,4 @@
-package io.smallrye.stork.test;
+package io.smallrye.stork;
 
 import static org.mockito.Mockito.mock;
 
